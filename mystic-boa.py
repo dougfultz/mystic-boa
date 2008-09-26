9ba0dd3793ce26df073c9af140eb563cba4a8b6e
Doug Fultz <dougfultz@cgdlub>
2008-09-26 15:08:02 -0400

    removed selective downloading, will now download all items in all enclosures

--- mystic-boa.py
+++ mystic-boa.py
@@ -8,7 +8,6 @@ import feedparser
 #initialization
 workingDir="/home/videos"
 feeds=["http://revision3.com/diggnation/feed/quicktime-high-definition","http://revision3.com/diggreel/feed/quicktime-high-definition","http://revision3.com/epicfu/feed/quicktime-large","http://revision3.com/hak5/feed/quicktime-large","http://revision3.com/internetsuperstar/feed/quicktime-high-definition","http://revision3.com/internetsuperstar/feed/quicktime-high-definition","http://revision3.com/rev3gazette/feed/quicktime-high-definition","http://revision3.com/scamschool/feed/quicktime-high-definition","http://revision3.com/systm/feed/quicktime-high-definition","http://revision3.com/tekzilla/feed/quicktime-high-definition","http://revision3.com/trs/feed/quicktime-high-definition","http://revision3.com/webdrifter/feed/quicktime-high-definition","http://revision3.com/winelibrarytv/feed/quicktime-high-definition"]
-itemTypes=["video/quicktime"]
 
 for curFeedNum in range(0,len(feeds)):
     os.chdir(workingDir)
@@ -27,8 +26,7 @@ for curFeedNum in range(0,len(feeds)):
     for item in range(0,len(curFeed['entries'])):
         print curFeed.entries[item].title
         for enc in range(0,len(curFeed.entries[item].enclosures)):
-            if (curFeed.entries[item].enclosures[enc].type in itemTypes):
-                print "Downloading - "+curFeed.entries[item].title
-                os.system("wget -c "+curFeed.entries[item].enclosures[enc].href)
-                print "Finished Downloading - "+curFeed.entries[item].title
+            print "Downloading - "+curFeed.entries[item].title
+            os.system("wget -c "+curFeed.entries[item].enclosures[enc].href)
+            print "Finished Downloading - "+curFeed.entries[item].title
         print "\n\n\n\n"
