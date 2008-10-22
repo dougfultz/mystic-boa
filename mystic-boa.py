fc73f0b051b4ccad27ac78103f79cb60a75a2842
Doug Fultz <dougfultz@cgdlub>
2008-10-22 15:18:00 -0400

    Completed blueprind change-downloading

--- mystic-boa.py
+++ mystic-boa.py
@@ -10,6 +10,8 @@ import ConfigParser
 configFile=os.getcwd()+"/mystic-boa.conf"
 ini=ConfigParser.SafeConfigParser()
 ini.read(configFile)
+tempDir=None
+errors=[]
 
 #----------------------------------------------------------------------------
 def checkCreateDir(path, dirName):
@@ -21,26 +23,44 @@ def checkCreateDir(path, dirName):
 	else:
 		print "\tDirectory Exists.\n"
 #----------------------------------------------------------------------------
+def getFileName(URL):
+	URL=URL.split('/')
+	return URL[len(URL)-1]
+#----------------------------------------------------------------------------
 def downloadFile(title,fileURL):
 	"""Output to user, download the file, output to user"""
+	global errors
+	rc=None
+	downloadDir=os.getcwd()
+	os.chdir(tempDir)
 	print "Downloading content - "+title
-	os.system("wget -c "+fileURL)
+	rc=os.system("wget -c "+fileURL)
+	if (rc==0):
+		try:
+			print "Moving "+getFileName(fileURL)+" from "+tempDir+" to "+downloadDir+"."
+			os.system("mv \""+tempDir+"/"+getFileName(fileURL)+"\" \""+downloadDir+"/\"")
+		except:
+			print "move failed"
+			errors.append(getFileName(fileURL)+"move failed")
 	print "Finished Downloading content - "+title
+	os.chdir(downloadDir)
 #----------------------------------------------------------------------------
 def processFeed(workingDir, curFeedURL):
 	"""Processes the current feed URL"""
 	os.chdir(workingDir)
 	print "Downloading and parsing - "+curFeedURL
 	curFeed=feedparser.parse(curFeedURL)
-    
+
 	checkCreateDir(workingDir,curFeed.feed.title)
 	os.chdir(workingDir+"/"+curFeed.feed.title)
         
 	#loop through entries in feed
 	for item in range(0,len(curFeed['entries'])):
 		for enc in range(0,len(curFeed.entries[item].enclosures)):
-			downloadFile(curFeed.entries[item].title,curFeed.entries[item].enclosures[enc].href)
-		print "\n\n\n\n"
+			if(os.path.exists(getFileName(curFeed.entries[item].enclosures[enc].href))==True):
+				print getFileName(curFeed.entries[item].enclosures[enc].href)+" already exists."
+			else:
+				downloadFile(curFeed.entries[item].title,curFeed.entries[item].enclosures[enc].href)
 #---------------------------------------------------------------------------
 def getSections():
 	"""Reads the config file and returns a list of the sections."""
@@ -48,6 +68,8 @@ def getSections():
 	for folder in ini.sections():
 		if (os.path.exists(folder)==True):
 			goodSections.append(folder)
+		elif (folder=="global"):
+			pass
 		else:
 			print "Section "+folder+" is not a valid path.  Please edit the config or create the directory."
 			os.system("sleep 60s")
@@ -74,10 +96,36 @@ def getFeeds(section):
 		pass
 	f.close()
 	return goodItems
+#----------------------------------------------------------------------------
+def setGlobals():
+	"""Checks all values from global section to verify that the config is correct."""
+	goodList=[]
+	global tempDir
+	tempDir=ini.get("global","tempDir")
+	if (os.path.exists(tempDir)==False):
+		print tempDir+" is not a valid path.  Please edit the config or create the directory."
+		goodList.append(False)
+		tempDir=None
+	for good in goodList:
+		if (good==False):
+			return good
+	return True
 #  MAIN  ####################################################################
 
 print "Mystic-Boa"
 
-for curSection in getSections():
-	for curFeed in getFeeds(curSection):
-		processFeed(curSection,curFeed)
+try:
+	if (setGlobals()==True):
+		for curSection in getSections():
+			for curFeed in getFeeds(curSection):
+				try:
+					processFeed(curSection,curFeed)
+				except AttributeError, detail:
+					print "An error has occured.\n"+curFeed
+					print detail
+					os.system("sleep 30s")
+					errors.append(curFeed+" has had the following error "+detail.args)
+except KeyboardInterrupt:
+	print "Canceled by user."
+
+print "Errors:\n",errors
